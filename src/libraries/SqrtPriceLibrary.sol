f2b2eecc15adc6b7a2ef611e8fcd45276eb120e7
saucepoint <saucepoint@protonmail.com>
2025-04-22 11:53:37 -0400

    additional helper functions

--- src/libraries/SqrtPriceLibrary.sol
+++ src/libraries/SqrtPriceLibrary.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
+
 library SqrtPriceLibrary {
     uint160 internal constant Q96 = 2 ** 96;
     uint256 internal constant Q192 = 2 ** 192;
+
+    /// @notice Given an exchange rate against ETH, return the sqrtPriceX96
+    /// @dev The exchange rate is in WAD format, e.g. 1e18 tokens exchange for 1.04e18 ETH
+    /// @param exchangeRateWad The exchange rate in WAD format
+    /// @return sqrtPriceX96 The sqrt price in sqrtX96 format
+    function exchangeRateToSqrtPriceX96(uint256 exchangeRateWad) internal pure returns (uint160 sqrtPriceX96) {
+        return fractionToSqrtPriceX96(1e18, exchangeRateWad);
+    }
+
+    /// @notice returns sqrt(numerator / denominator) * Q96
+    function fractionToSqrtPriceX96(uint256 numerator, uint256 denominator)
+        internal
+        pure
+        returns (uint160 sqrtPriceX96)
+    {
+        // sqrtPriceX96 = sqrt(numerator / denominator) * Q96)
+        // sqrtPriceX96 = sqrt(numerator) * 2^96 / sqrt(denominator)
+        return uint160(FixedPointMathLib.sqrt(numerator) * Q96 / FixedPointMathLib.sqrt(denominator));
+    }
 
     /// @notice Calculates the absolute difference between two sqrt prices
     function absDifferenceX96(uint160 sqrtPriceAX96, uint160 sqrtPriceBX96) internal pure returns (uint160) {
